051589af4b10fb4e286feb9c41767335b387b29b
cgdevopsgcp <97515980+cgdevopsgcp@users.noreply.github.com>
2022-02-21 17:50:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable "region" {
+default = "us-central1-a" 
+}
+provider "google" {
+credentials = "${file("service.json")}"
+project = "cg-anz-practice"
+region = "${var.region}"
+}
+resource "google_compute_instance" "test" {
+count = 1 // Adjust as desired
+name = "test${count.index + 1}" // yields "test1", "test2", etc. It's also
+machine_type = "f1-micro" // smallest (CPU & RAM) available instance
+zone = "${var.region}" // yields "europe-west1-d" as setup previously.
+boot_disk {
+initialize_params {
+image = "debian-7-wheezy-v20160301" 
+}
+}
+network_interface {
+network = "default"
+access_config {
+}
+}
